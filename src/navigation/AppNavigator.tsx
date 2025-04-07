20b90a56c1e500fb334f4cc95b6512b6ae255e18
Shimra Safwan <fshimra2000@gmail.com>
2025-04-07 20:18:10 +0530

    Added the Home Screen

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,8 +1,13 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-import ChatScreen from '../screens/ChatScreen'; // Verify this path is correct
+import ChatScreen from '../screens/ChatScreen';
+import HomeScreen from '../screens/HomeScreen'; 
+import RecordingScreen from '../screens/RecordingScreen'; 
 
+// Define your route params
 export type RootStackParamList = {
+  Home: undefined;
+  Record: undefined;
   Chat: undefined;
 };
 
@@ -11,10 +16,20 @@ const Stack = createStackNavigator<RootStackParamList>();
 const AppNavigator = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{ title: 'Home' }} // Customize as needed
+        />
+         <Stack.Screen 
+          name="Record" 
+          component={RecordingScreen} 
+          options={{ title: 'Record' }}
+        />
         <Stack.Screen 
           name="Chat" 
-          component={ChatScreen} // Make sure this is the correct component
+          component={ChatScreen}
           options={{ title: 'Doctor-Patient Chat' }}
         />
       </Stack.Navigator>
